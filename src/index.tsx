f9c729bfe28b741606a8eb002ecbc11357fe8011
Siarhei Herasko <siarhei-herasko@yandex.by>
2022-10-19 14:25:22 +0300

    Connect darkTheme

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import {CssBaseline, ThemeProvider} from '@mui/material';
 
 import store from 'redux/store';
+import {darkTheme} from 'configs/theme';
 
 import Application from 'application';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
         <BrowserRouter>
-            <Application/>
+            <ThemeProvider theme={darkTheme}>
+                <Application/>
+                <CssBaseline/>
+            </ThemeProvider>
         </BrowserRouter>
     </Provider>
 );
