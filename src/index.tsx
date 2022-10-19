de6644e1e39f0cc9606b6aa2052f2eda6602e331
Siarhei Herasko <siarhei-herasko@yandex.by>
2022-10-19 14:15:07 +0300

    Connect React Router Dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 
 import store from 'redux/store';
 
@@ -8,6 +9,8 @@ import Application from 'application';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
-        <Application/>
+        <BrowserRouter>
+            <Application/>
+        </BrowserRouter>
     </Provider>
 );
