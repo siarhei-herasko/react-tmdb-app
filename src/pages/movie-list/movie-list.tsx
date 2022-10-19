bb0c84177a5eb182ec0d70448ba351c54497aa8e
Siarhei Herasko <siarhei-herasko@yandex.by>
2022-10-19 17:54:50 +0300

    Add MovieList and TVList api

--- src/pages/movie-list/movie-list.tsx
+++ src/pages/movie-list/movie-list.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+import useRequest from 'hooks/use-request';
+import {getMovieList} from './movie-list.api';
+import {movieListDefaultFilter} from './movie-list.configs';
 
 const MovieList = () => {
+    const {data, loading, error, run} = useRequest(getMovieList);
+    useEffect(() => {
+        run(movieListDefaultFilter);
+    }, [run]);
+    console.log(data, loading, error);
     return (
         <div>
             MovieList
--- src/pages/tv-list/tv-list.tsx
+++ src/pages/tv-list/tv-list.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+import useRequest from 'hooks/use-request';
+import {getTVList} from './tv-list.api';
+import {tvListDefaultFilter} from './tv-list.configs';
 
 const TVList = () => {
+    const {data, loading, error, run} = useRequest(getTVList);
+    useEffect(() => {
+        run(tvListDefaultFilter);
+    }, [run]);
+    console.log(data, loading, error);
     return (
         <div>
             TVList
