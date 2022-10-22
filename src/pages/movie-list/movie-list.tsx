f84539e53d7ea35e9b8657634d92441497b23da4
Siarhei Herasko <siarhei-herasko@yandex.by>
2022-10-22 13:17:42 +0300

    Edit MovieList ant TVList

--- src/pages/movie-list/movie-list.tsx
+++ src/pages/movie-list/movie-list.tsx
@@ -1,19 +1,31 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
 
+import useFilter from 'hooks/use-filter';
 import useRequest from 'hooks/use-request';
 import {getMovieList} from './movie-list.api';
-import {movieListDefaultFilter} from './movie-list.configs';
+import {movieListDefaultFilter, movieListSortingList} from './movie-list.configs';
+
+import Accordion from 'components/accordion';
+import SortingList from 'components/sorting-list';
+import GenreList from 'components/genre-list';
+import CardList from 'components/card-list';
+import Pagination from 'components/pagination';
 
 const MovieList = () => {
+    const {filter, setSorting, toggleGenre, setPage} = useFilter(movieListDefaultFilter);
     const {data, loading, error, run} = useRequest(getMovieList);
     useEffect(() => {
-        run(movieListDefaultFilter);
-    }, [run]);
-    console.log(data, loading, error);
+        run(filter);
+    }, [run, filter]);
     return (
-        <div>
-            MovieList
-        </div>
+        <Fragment>
+            <Accordion name="Filter">
+                <SortingList list={movieListSortingList} active={filter.sorting} onClick={setSorting}/>
+                <GenreList type="movie" actives={filter.genres} onClick={toggleGenre}/>
+            </Accordion>
+            <CardList list={data?.list || []} loading={loading} error={error}/>
+            <Pagination total={data?.total || 0} active={filter.page} onClick={setPage}/>
+        </Fragment>
     );
 };
 
--- src/pages/tv-list/tv-list.tsx
+++ src/pages/tv-list/tv-list.tsx
@@ -1,19 +1,31 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
 
+import useFilter from 'hooks/use-filter';
 import useRequest from 'hooks/use-request';
 import {getTVList} from './tv-list.api';
-import {tvListDefaultFilter} from './tv-list.configs';
+import {tvListDefaultFilter, tvListSortingList} from './tv-list.configs';
+
+import Accordion from 'components/accordion';
+import SortingList from 'components/sorting-list';
+import GenreList from 'components/genre-list';
+import CardList from 'components/card-list';
+import Pagination from 'components/pagination';
 
 const TVList = () => {
+    const {filter, setSorting, toggleGenre, setPage} = useFilter(tvListDefaultFilter);
     const {data, loading, error, run} = useRequest(getTVList);
     useEffect(() => {
-        run(tvListDefaultFilter);
-    }, [run]);
-    console.log(data, loading, error);
+        run(filter);
+    }, [run, filter]);
     return (
-        <div>
-            TVList
-        </div>
+        <Fragment>
+            <Accordion name="Filter">
+                <SortingList list={tvListSortingList} active={filter.sorting} onClick={setSorting}/>
+                <GenreList type="tv" actives={filter.genres} onClick={toggleGenre}/>
+            </Accordion>
+            <CardList list={data?.list || []} loading={loading} error={error}/>
+            <Pagination total={data?.total || 0} active={filter.page} onClick={setPage}/>
+        </Fragment>
     );
 };
 
