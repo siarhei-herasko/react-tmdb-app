3c0e6aed16b16d6212aa0a46375ce9cc76d2ca74
Siarhei Herasko <siarhei-herasko@yandex.by>
2022-10-23 17:58:44 +0300

    Add MovieDetails and TVDetails api

--- src/pages/movie-details/movie-details.tsx
+++ src/pages/movie-details/movie-details.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+
+import useRequest from 'hooks/use-request';
+import {getMovieDetails} from './movie-details.api';
 
 const MovieDetails = () => {
+    const {data, loading, error, run} = useRequest(getMovieDetails);
+    const {id} = useParams();
+    useEffect(() => {
+        if (id) {
+            run(Number(id));
+        }
+    }, [run, id]);
+    console.log(data, loading, error);
     return (
         <div>
             MovieDetails
--- src/pages/tv-details/tv-details.tsx
+++ src/pages/tv-details/tv-details.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+
+import useRequest from 'hooks/use-request';
+import {getTVDetails} from './tv-details.api';
 
 const TVDetails = () => {
+    const {data, loading, error, run} = useRequest(getTVDetails);
+    const {id} = useParams();
+    useEffect(() => {
+        if (id) {
+            run(Number(id));
+        }
+    }, [run, id]);
+    console.log(data, loading, error);
     return (
         <div>
             TVDetails
