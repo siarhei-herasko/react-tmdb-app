c3d0dfe73aa122994919cc60b487301dfdbdb3e3
Siarhei Herasko <siarhei-herasko@yandex.by>
2022-10-23 14:14:06 +0300

    Edit Card

--- src/components/card-list/components/card/card.tsx
+++ src/components/card-list/components/card/card.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
-import {Card as MUICard, CardContent, CardMedia, Typography} from '@mui/material';
+import {Card as MUICard, CardActionArea, CardContent, Typography} from '@mui/material';
 
 import {Path} from 'routing';
 import {CardProps} from './card.types';
+
+import LazyImage from 'components/lazy-image';
 
 const Card = ({type, id, title, poster}: CardProps) => {
     const navigate = useNavigate();
@@ -13,12 +15,14 @@ const Card = ({type, id, title, poster}: CardProps) => {
     };
     return (
         <MUICard onClick={goToDetails} variant="outlined">
-            <CardMedia image={poster || undefined} component="img" sx={{aspectRatio: '3/4'}}/>
-            <CardContent>
-                <Typography variant="h2" fontSize="1rem" fontWeight="medium" align="center" noWrap>
-                    {title}
-                </Typography>
-            </CardContent>
+            <CardActionArea>
+                <LazyImage src={poster} alt={title} aspectRatio="3/4"/>
+                <CardContent>
+                    <Typography variant="h2" fontSize="1rem" fontWeight="medium" align="center" noWrap>
+                        {title}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
         </MUICard>
     );
 };
